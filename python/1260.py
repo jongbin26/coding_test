4e2e8dcd4a05c70b9ff806ea9be0f0a88b6268ce
종빈 <jongbin2326@hanmail.net>
2022-07-02 14:24:27 +0000

    백준 알고리즘 문제 풀이

---
+++ python/1260.py
@@ -0,0 +1,36 @@
+from collections import deque #for BFS
+n, m, v = map(int, input().split())
+
+graph = [[0]*(n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b]=graph[b][a]=1
+    
+def dfs(v):
+    visited[v]=1
+    print(v, end=' ')
+    for i in range(n+1):
+        if visited[i]==0 and graph[v][i]==1:
+            dfs(i)
+
+            
+def bfs(v):
+    queue = deque()
+    queue.append(v)
+    visited[v]=1
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in range(n+1):
+            if visited[i]==0 and graph[v][i]==1:
+                queue.append(i)
+                visited[i]=1
+
+visited=[0]*(n+1)           
+dfs(v)
+
+print("")
+
+visited=[0]*(n+1)
+bfs(v)
