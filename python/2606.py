89918db65e21ad3eca9c3f483f520a9700652c39
종빈 <jongbin2326@hanmail.net>
2022-07-03 14:15:28 +0000

    백준 알고리즘 풀이

---
+++ python/2606.py
@@ -0,0 +1,26 @@
+from collections import deque
+n = int(input())
+v = int(input())
+
+graph = [[0]*(n+1) for _ in range(n+1)]
+
+for _ in range(v):
+    a, b = map(int, input().split())
+    graph[a][b] = graph[b][a] = 1
+    
+visited = [0] * (n+1)
+
+global total
+total=0
+
+def dfs(v):
+    visited[v] = 1
+    global total
+    # print(total, end =' ')
+    for i in range(n+1):
+        if graph[v][i]==1 and visited[i] == 0:
+            total+=1
+            dfs(i)
+    return total
+
+print(dfs(1))
