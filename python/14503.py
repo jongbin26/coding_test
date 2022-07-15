9eb5bb668a121f7baab7b773535fd087303a767b
종빈 <010jjj@naver.com>
2022-07-15 13:31:24 +0000

    백준 알고리즘 문제 풀이

--- python/14503.py
+++ python/14503.py
@@ -6,40 +6,46 @@ def turn(d):
     d -= 1
     if d < 0:
         d += 4
-    return d
-    
+    return d        
+
 def dfs(x, y, d):
-    visited[x][y] = 1
+    global total
+    if visited[x][y] == 0 and graph[x][y] == 0:
+        visited[x][y] = 1
+        total += 1
     
     for _ in range(4):
-        d = turn(d)
-        nx = x + dx[d]
-        ny = y + dy[d]
-        
+        nd = turn(d)
+        nx = x + dx[nd]
+        ny = y + dy[nd]
         if visited[nx][ny] == 0 and graph[nx][ny] == 0:
-            visited[nx][ny] = 1
-            dfs(nx, ny, d)
+            dfs(nx, ny, nd)
+            return
+        d = nd
+    nd = turn(turn(d))
+    nx = x + dx[nd]
+    ny = y + dy[nd]
+    if graph[nx][ny] == 1:
+        return
+    dfs(nx, ny, d)
     
-        
+
+
 
 n, m = map(int, input().split())
-r, c, d = map(int, input().split())
+x, y, d = map(int, input().split())
 
 graph = []
 visited = []
+total = 0
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]	
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]	
 
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
 visited = [[0 for _ in range(m)]for _ in range(n)]
 
-dfs(r, c, d)
-total = 0
-for row in visited:
-    for j in row:
-        if j == 1:
-            total += 1
+dfs(x, y, d)
 print(total)
