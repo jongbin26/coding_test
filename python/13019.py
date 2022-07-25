156abcd1cdd1179d0a05612e6b0b839a67a11d4a
jongbin cho <81405795+jongbin26@users.noreply.github.com>
2022-07-25 23:47:10 +0900

    백준 알고리즘 문제 풀이
    백준 알고리즘 문제 풀이

---
+++ python/13019.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+ 
+if __name__ == "__main__":
+    A = input()
+    B = input()
+    ans = 0
+    if sorted(A) != sorted(B):
+        print(-1)
+        exit()
+    curr = len(A)-1
+    if curr == 0 :
+        if A == B:
+            print(0)
+        else:
+            print(-1)
+        exit()
+    for i in range(len(A)-1,-1,-1):
+        if A[i] != B[curr]:
+            ans += 1
+        else:
+            curr -= 1
+    print(ans)
