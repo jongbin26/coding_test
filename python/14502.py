b5ecaf4cdc50d8ade6d0bc89924b9810e81c0453
종빈 <010jjj@naver.com>
2022-09-24 08:56:12 +0000

    백준 알고리즘 문제 풀이

--- python/14502.py
+++ python/14502.py
@@ -1,54 +1,46 @@
+from itertools import combinations
+import copy
 n, m = map(int, input().split())
-data = []
-temp = [[0] * m for _ in range(n)]
-
+graph = []
 for _ in range(n):
-    data.append(list(map(int, input().split())))
-    
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+    graph.append(list(map(int, input().split())))
+zero_list = []
 
-result = 0
+#0 좌표 구하기
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            zero_list.append([i, j])
+#조합
+case = list(combinations(zero_list, 3))
 
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 def virus(x, y):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if nx >= 0 and nx < n and ny >= 0 and ny < m:
-            if temp[nx][ny] == 0:
-                temp[nx][ny] = 2
+        if nx >=0 and nx < n and ny >= 0 and ny < m:
+            if tmp[nx][ny] == 0:
+                tmp[nx][ny] = 2
                 virus(nx, ny)
 
-def get_score():
-    score = 0
-    for i in range(n):
-        for j in range(m):
-            if temp[i][j] == 0:
-                score += 1
-    return score
-
-def dfs(count):
-    global result
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = data[i][j]
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    virus(i, j)
-        
-        result = max(result, get_score())
-        return
+ans = 0
+for wall_list in case:
+    result = 0
+    tmp = copy.deepcopy(graph)
+    for wall in wall_list:
+        tmp[wall[0]][wall[1]] = 1
     
     for i in range(n):
         for j in range(m):
-            if data[i][j] == 0:
-                data[i][j] = 1
-                count += 1
-                dfs(count)
-                data[i][j] = 0
-                count -= 1
-                
-dfs(0)
-print(result)
+            if tmp[i][j] == 2:
+                virus(i, j)
+    
+    for i in range(n):
+        for j in range(m):
+            if tmp[i][j] == 0:
+                result += 1
+    
+    ans = max(result, ans)
+print(ans)
