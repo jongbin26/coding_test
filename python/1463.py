933f60c453b8e4d53dd4ddc30bfee7ef6a69a40c
종빈 <010jjj@naver.com>
2022-10-01 05:01:17 +0000

    백준 알고리즘 문제 풀이

---
+++ python/1463.py
@@ -0,0 +1,11 @@
+n = int(input())
+nums = [0] * (n+1)
+
+for i in range(2, n+1):
+    nums[i] = nums[i-1] + 1
+    if i % 3 == 0:
+        nums[i] = min(nums[i], nums[i//3] + 1)
+    if i % 2 == 0:
+        nums[i] = min(nums[i], nums[i//2] + 1)
+        
+print(nums[n])
