4cfa5959b310b9bb569314f5fbd9a84ad9508894
종빈 <010jjj@naver.com>
2022-10-28 14:23:24 +0000

    백준 알고리즘 문제 풀이

--- python/2293.py
+++ python/2293.py
@@ -2,11 +2,8 @@ n, k = map(int, input().split())
 coin = list(int(input()) for _ in range(n))
 
 dp = [0] * (k+1)
-
+dp[0] = 1
 for i in range(1, n+1):
-    for j in range(1, k+1):
-        if j == coin[i-1]:
-            dp[j] += 1
-        elif j > coin[i-1]:
+    for j in range(coin[i-1], k+1):
             dp[j] += dp[j-coin[i-1]]
 print(dp[k])
