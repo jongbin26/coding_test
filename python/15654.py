a7699c816a9216738aa722735e4120632d62b045
종빈 <010jjj@naver.com>
2022-11-04 13:33:55 +0000

    백준 알고리즘 문제 풀이

---
+++ python/15654.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+s = []
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(n):
+        if arr[i] not in s:
+            s.append(arr[i])
+            dfs()
+            s.pop()
+dfs()
