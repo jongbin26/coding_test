873ba20ca3f103faf904ddd20a677f3cb6c01ca9
종빈 <010jjj@naver.com>
2022-11-12 09:47:38 +0000

    백준 알고리즘 문제 풀이

---
+++ python/2493.py
@@ -0,0 +1,23 @@
+n = int(input())
+temp = list(map(int, input().split()))
+tower = []
+for i in range(len(temp)):
+    tower.append([i, temp[i]])
+
+stack = [[tower[0][0], tower[0][1]]]
+ans = [0] * n
+for i in range(1, len(tower)):
+    while(True):
+        if stack:
+            if stack[-1][1] < tower[i][1]:
+                stack.pop()
+            else:
+                ans[i] = stack[-1][0] + 1
+                stack.append([tower[i][0], tower[i][1]])
+                break
+        else:
+            ans[i] = 0
+            stack.append([tower[i][0], tower[i][1]])
+            break
+for i in ans:
+    print(i, end=' ')
