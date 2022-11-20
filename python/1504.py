c98c24ff35fc5e6134315cfa18a39b11ec143267
종빈 <010jjj@naver.com>
2022-11-20 10:08:49 +0000

    백준 알고리즘 문제 풀이

---
+++ python/1504.py
@@ -0,0 +1,44 @@
+import heapq
+INF = int(1e9)
+n, e = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+v1, v2 = map(int, input().split())
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while(q):
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                heapq.heappush(q, (cost, i[0]))
+                distance[i[0]] = cost
+
+distance = [INF] * (n+1)
+dijkstra(1)
+a1 = distance[v1]
+b1 = distance[v2]
+
+distance = [INF] * (n+1)
+dijkstra(v1)
+a2 = distance[v2]
+b3 = distance[n]
+
+distance = [INF] * (n+1)
+dijkstra(v2)
+a3 = distance[n]
+b2 = distance[v1]
+
+short = min(a1+a2+a3, b1+b2+b3)
+if short >= int(1e9):
+    print(-1)
+else:
+    print(short)
