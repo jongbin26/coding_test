1cf10e1c190ce865f807c7cb5d15c114752353e8
종빈 <010jjj@naver.com>
2023-01-04 14:07:33 +0000

    백준 알고리즘 문제 풀이

--- python/13335.py
+++ python/13335.py
@@ -1,16 +1,15 @@
-from collections import deque
 n, w, l = map(int, input().split())
-arr = list(map(int, input().split()))
-weight, cnt, ans = 0, 0, 0
-for i in arr:
-    if weight + i < l:
-        weight += i
-        cnt += 1
-        continue
-    else:
-        ans += cnt + w
-        weight, cnt = 0, 0
-        weight += i
-        cnt += 1
-ans += cnt
-print(ans)
+trucks = list(map(int, input().split()))
+ 
+bridge = [0] * w
+time = 0
+ 
+while bridge:
+    time += 1
+    bridge.pop(0)
+    if trucks:
+        if sum(bridge) + trucks[0] <= l:
+            bridge.append(trucks.pop(0))
+        else:
+            bridge.append(0)
+print(time)
