e4e03cddddbb9e2d33fa86196ee758614d842c74
종빈 <010jjj@naver.com>
2023-01-07 07:21:55 +0000

    백준 알고리즘 문제 풀이

--- python/2075.py
+++ python/2075.py
@@ -1,15 +1,30 @@
+# import heapq
+# n = int(input())
+# graph = [list(map(int, input().split())) for _ in range(n)]
+# heap = []
+# for i in range(n):
+#     for j in range(n):
+#         if len(heap) < n:
+#             heapq.heappush(heap, graph[i][j])
+#         else:
+#             if heap[0] > graph[i][j]:
+#                 continue
+#             else:
+#                 heapq.heappop(heap)
+#                 heapq.heappush(heap, graph[i][j])
+# print(heap[0])
+
 import heapq
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-heap = []
 for i in range(n):
-    for j in range(n):
-        if len(heap) < n:
-            heapq.heappush(heap, graph[i][j])
+    graph = list(map(int, input().split()))
+    if i == 0:
+        heap = graph
+        continue
+    for j in graph:
+        if j < heap[0]:
+            continue
         else:
-            if heap[0] > graph[i][j]:
-                continue
-            else:
-                heapq.heappop(heap)
-                heapq.heappush(heap, graph[i][j])
-print(heap[0])
+            heapq.heappop(heap)
+            heapq.heappush(heap, j)
+print(heap)
