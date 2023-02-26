663df538acae1d1f59a424635b18962da2bcb47e
종빈 <010jjj@naver.com>
2023-02-26 14:05:20 +0000

    백준 알고리즘 문제 풀이

---
+++ python/1106.py
@@ -0,0 +1,11 @@
+c, n = map(int, input().split())
+infos = [list(map(int, input().split())) for _ in range(n)]
+infos.sort(key = lambda x:x[1])
+dp = [int(1e9)] * (c+infos[-1][1]+1)
+for info in infos:
+    dp[info[1]] = min(dp[info[1]], info[0])
+for info in infos:
+    for i in range(info[1], c+infos[-1][1]+1):
+        if i-info[1] > 0:
+            dp[i] = min(dp[i], dp[i-info[1]] + info[0])
+print(min(dp[c:]))
