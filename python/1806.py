adc1731b138fd66c0329082842e74ba64814eb77
jongbin26 <010jjj@naver.com>
2024-05-09 14:44:15 +0900

    백준 알고리즘 문제 풀이

--- python/1806.py
+++ python/1806.py
@@ -1,14 +1,20 @@
 n, s = map(int ,input().split())
 arr = list(map(int, input().split()))
 
-if len(arr) >= 2:
-    left, right = 0, 1
-sum = arr[left] + arr[right]
+left, right = 0, 0
+sum = 0
+min_length = 1e9
 while(True):
-    if arr[left] + arr[right] + arr[right+1] < s:
-        right += 1
-        sum = arr[left] + arr[right] + arr[right+1]
-    else:
-        arr -= sum[left]
+    if sum >= s:
+        min_length = min(right-left, min_length)
+        sum -= arr[left]
         left += 1
+    elif right == n:
+        break
+    else:
+        sum += arr[right]
+        right += 1
+if min_length == 1e9:
+    print(0)
+else: print(min_length)
     
