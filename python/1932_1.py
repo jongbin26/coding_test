d7afdf512d306d7a7ca4752bd3a82fa76811207e
jongbin26 <010jjj@naver.com>
2024-07-19 14:30:51 +0900

    백준 알고리즘 문제 풀이

---
+++ python/1932_1.py
@@ -0,0 +1,16 @@
+n = int(input())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = graph[0][0]
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            dp[i][j] = graph[i][j] + dp[i-1][j]
+        elif j == i:
+            dp[i][j] = graph[i][j] + dp[i-1][j-1]
+        else:
+            dp[i][j] = graph[i][j] + max(dp[i-1][j], dp[i-1][j-1])
+print(dp[n-1])
+print(dp[1])
